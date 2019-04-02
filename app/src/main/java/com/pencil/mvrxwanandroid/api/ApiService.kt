1fd2109807c401f0456e29c6b815a8967eae5c2e
landingpencil <landingpencil@gmail.com>
2019-04-03 05:14:31 +0800

    init

---
+++ app/src/main/java/com/pencil/mvrxwanandroid/api/ApiService.kt
@@ -0,0 +1,279 @@
+package com.pencil.mvrxwanandroid.api
+
+import io.reactivex.Observable
+import retrofit2.http.*
+
+interface ApiService {
+
+    /**
+     * 获取轮播图
+     * http://www.wanandroid.com/banner/json
+     */
+    @GET("banner/json")
+    fun getBanners(): Observable<HttpResult<List<Banner>>>
+
+    /**
+     * 获取首页置顶文章列表
+     * http://www.wanandroid.com/article/top/json
+     */
+    @GET("article/top/json")
+    fun getTopArticles(): Observable<HttpResult<MutableList<Article>>>
+
+    /**
+     * 获取文章列表
+     * http://www.wanandroid.com/article/list/0/json
+     * @param pageNum
+     */
+    @GET("article/list/{pageNum}/json")
+    fun getArticles(@Path("pageNum") pageNum: Int): Observable<HttpResult<ArticleResponseBody>>
+
+    /**
+     * 获取知识体系
+     * http://www.wanandroid.com/tree/json
+     */
+    @GET("tree/json")
+    fun getKnowledgeTree(): Observable<HttpResult<List<KnowledgeTreeBody>>>
+
+    /**
+     * 知识体系下的文章
+     * http://www.wanandroid.com/article/list/0/json?cid=168
+     * @param page
+     * @param cid
+     */
+    @GET("article/list/{page}/json")
+    fun getKnowledgeList(@Path("page") page: Int, @Query("cid") cid: Int): Observable<HttpResult<ArticleResponseBody>>
+
+    /**
+     * 导航数据
+     * http://www.wanandroid.com/navi/json
+     */
+    @GET("navi/json")
+    fun getNavigationList(): Observable<HttpResult<List<NavigationBean>>>
+
+    /**
+     * 项目数据
+     * http://www.wanandroid.com/project/tree/json
+     */
+    @GET("project/tree/json")
+    fun getProjectTree(): Observable<HttpResult<List<ProjectTreeBean>>>
+
+    /**
+     * 项目列表数据
+     * http://www.wanandroid.com/project/list/1/json?cid=294
+     * @param page
+     * @param cid
+     */
+    @GET("project/list/{page}/json")
+    fun getProjectList(@Path("page") page: Int, @Query("cid") cid: Int): Observable<HttpResult<ArticleResponseBody>>
+
+    /**
+     * 登录
+     * http://www.wanandroid.com/user/login
+     * @param username
+     * @param password
+     */
+    @POST("user/login")
+    @FormUrlEncoded
+    fun loginWanAndroid(@Field("username") username: String,
+                        @Field("password") password: String): Observable<HttpResult<LoginData>>
+
+    /**
+     * 注册
+     * http://www.wanandroid.com/user/register
+     * @param username
+     * @param password
+     * @param repassword
+     */
+    @POST("user/register")
+    @FormUrlEncoded
+    fun registerWanAndroid(@Field("username") username: String,
+                           @Field("password") password: String,
+                           @Field("repassword") repassword: String): Observable<HttpResult<LoginData>>
+
+    /**
+     * 退出登录
+     * http://www.wanandroid.com/user/logout/json
+     */
+    @GET("user/logout/json")
+    fun logout(): Observable<HttpResult<Any>>
+
+    /**
+     *  获取收藏列表
+     *  http://www.wanandroid.com/lg/collect/list/0/json
+     *  @param page
+     */
+    @GET("lg/collect/list/{page}/json")
+    fun getCollectList(@Path("page") page: Int): Observable<HttpResult<CollectionResponseBody<CollectionArticle>>>
+
+    /**
+     * 收藏站内文章
+     * http://www.wanandroid.com/lg/collect/1165/json
+     * @param id article id
+     */
+    @POST("lg/collect/{id}/json")
+    fun addCollectArticle(@Path("id") id: Int): Observable<HttpResult<Any>>
+
+    /**
+     * 收藏站外文章
+     * http://www.wanandroid.com/lg/collect/add/json
+     * @param title
+     * @param author
+     * @param link
+     */
+    @POST("lg/collect/add/json")
+    @FormUrlEncoded
+    fun addCoolectOutsideArticle(@Field("title") title: String,
+                                 @Field("author") author: String,
+                                 @Field("link") link: String): Observable<HttpResult<Any>>
+
+    /**
+     * 文章列表中取消收藏文章
+     * http://www.wanandroid.com/lg/uncollect_originId/2333/json
+     * @param id
+     */
+    @POST("lg/uncollect_originId/{id}/json")
+    fun cancelCollectArticle(@Path("id") id: Int): Observable<HttpResult<Any>>
+
+    /**
+     * 收藏列表中取消收藏文章
+     * http://www.wanandroid.com/lg/uncollect/2805/json
+     * @param id
+     * @param originId
+     */
+    @POST("lg/uncollect/{id}/json")
+    @FormUrlEncoded
+    fun removeCollectArticle(@Path("id") id: Int,
+                             @Field("originId") originId: Int = -1): Observable<HttpResult<Any>>
+
+    /**
+     * 搜索热词
+     * http://www.wanandroid.com/hotkey/json
+     */
+    @GET("hotkey/json")
+    fun getHotSearchData(): Observable<HttpResult<MutableList<HotSearchBean>>>
+
+    /**
+     * 搜索
+     * http://www.wanandroid.com/article/query/0/json
+     * @param page
+     * @param key
+     */
+    @POST("article/query/{page}/json")
+    @FormUrlEncoded
+    fun queryBySearchKey(@Path("page") page: Int,
+                         @Field("k") key: String): Observable<HttpResult<ArticleResponseBody>>
+
+    /**
+     * 获取TODO列表数据
+     * http://wanandroid.com/lg/todo/list/0/json
+     * @param type
+     */
+    @POST("/lg/todo/list/{type}/json")
+    fun getTodoList(@Path("type") type: Int): Observable<HttpResult<AllTodoResponseBody>>
+
+    /**
+     * 获取未完成Todo列表
+     * http://wanandroid.com/lg/todo/listnotdo/0/json/1
+     * @param type 类型拼接在链接上，目前支持0,1,2,3
+     * @param page 拼接在链接上，从1开始
+     */
+    @POST("/lg/todo/listnotdo/{type}/json/{page}")
+    fun getNoTodoList(@Path("page") page: Int, @Path("type") type: Int): Observable<HttpResult<TodoResponseBody>>
+
+    /**
+     * 获取已完成Todo列表
+     * http://www.wanandroid.com/lg/todo/listdone/0/json/1
+     * @param type 类型拼接在链接上，目前支持0,1,2,3
+     * @param page 拼接在链接上，从1开始
+     */
+    @POST("/lg/todo/listdone/{type}/json/{page}")
+    fun getDoneList(@Path("page") page: Int, @Path("type") type: Int): Observable<HttpResult<TodoResponseBody>>
+
+    /**
+     * V2版本 ： 获取TODO列表数据
+     * http://www.wanandroid.com/lg/todo/v2/list/页码/json
+     * @param page 页码从1开始，拼接在 url 上
+     * @param map
+     *          status 状态， 1-完成；0未完成; 默认全部展示；
+     *          type 创建时传入的类型, 默认全部展示
+     *          priority 创建时传入的优先级；默认全部展示
+     *          orderby 1:完成日期顺序；2.完成日期逆序；3.创建日期顺序；4.创建日期逆序(默认)；
+     */
+    @GET("/lg/todo/v2/list/{page}/json")
+    fun getTodoList(@Path("page") page: Int, @QueryMap map: MutableMap<String, Any>): Observable<HttpResult<AllTodoResponseBody>>
+
+    /**
+     * 仅更新完成状态Todo
+     * http://www.wanandroid.com/lg/todo/done/80/json
+     * @param id 拼接在链接上，为唯一标识
+     * @param status 0或1，传1代表未完成到已完成，反之则反之
+     */
+    @POST("/lg/todo/done/{id}/json")
+    @FormUrlEncoded
+    fun updateTodoById(@Path("id") id: Int, @Field("status") status: Int): Observable<HttpResult<Any>>
+
+    /**
+     * 删除一条Todo
+     * http://www.wanandroid.com/lg/todo/delete/83/json
+     * @param id
+     */
+    @POST("/lg/todo/delete/{id}/json")
+    fun deleteTodoById(@Path("id") id: Int): Observable<HttpResult<Any>>
+
+    /**
+     * 新增一条Todo
+     * http://www.wanandroid.com/lg/todo/add/json
+     * @param body
+     *          title: 新增标题
+     *          content: 新增详情
+     *          date: 2018-08-01
+     *          type: 0
+     */
+    @POST("/lg/todo/add/json")
+    @FormUrlEncoded
+    fun addTodo(@FieldMap map: MutableMap<String, Any>): Observable<HttpResult<Any>>
+
+    /**
+     * 更新一条Todo内容
+     * http://www.wanandroid.com/lg/todo/update/83/json
+     * @param body
+     *          title: 新增标题
+     *          content: 新增详情
+     *          date: 2018-08-01
+     *          status: 0 // 0为未完成，1为完成
+     *          type: 0
+     */
+    @POST("/lg/todo/update/{id}/json")
+    @FormUrlEncoded
+    fun updateTodo(@Path("id") id: Int, @FieldMap map: MutableMap<String, Any>): Observable<HttpResult<Any>>
+
+    /**
+     * 获取公众号列表
+     * http://wanandroid.com/wxarticle/chapters/json
+     */
+    @GET("/wxarticle/chapters/json")
+    fun getWXChapters(): Observable<HttpResult<MutableList<WXChapterBean>>>
+
+    /**
+     * 查看某个公众号历史数据
+     * http://wanandroid.com/wxarticle/list/405/1/json
+     * @param id 公众号 ID
+     * @param page 公众号页码
+     */
+    @GET("/wxarticle/list/{id}/{page}/json")
+    fun getWXArticles(@Path("id") id: Int,
+                      @Path("page") page: Int): Observable<HttpResult<ArticleResponseBody>>
+
+    /**
+     * 在某个公众号中搜索历史文章
+     * http://wanandroid.com/wxarticle/list/405/1/json?k=Java
+     * @param id 公众号 ID
+     * @param key 搜索关键字
+     * @param page 公众号页码
+     */
+    @GET("/wxarticle/list/{id}/{page}/json")
+    fun queryWXArticles(@Path("id") id: Int,
+                        @Query("k") key: String,
+                        @Path("page") page: Int): Observable<HttpResult<ArticleResponseBody>>
+
+}
---
+++ app/src/main/java/com/pencil/mvrxwanandroid/api/DataRepo.kt
@@ -0,0 +1,276 @@
+package com.pencil.mvrxwanandroid.api
+
+import com.squareup.moshi.Json
+import java.io.Serializable
+
+
+open class BaseBean{
+    var errorCode : Int = 0
+    var errorMsg : String = ""
+}
+//data class HttpResult<T>(@Json(name = "data") val data: T,
+//                         @Json(name = "errorCode") val errorCode: Int,
+//                         @Json(name = "errorMsg") val errorMsg: String)
+
+
+
+
+data class HttpResult<T>(@Json(name = "data") val data : T) : BaseBean()
+
+//文章
+data class ArticleResponseBody(
+    @Json(name = "curPage") val curPage: Int,
+    @Json(name = "datas") var datas: MutableList<Article>,
+    @Json(name = "offset") val offset: Int,
+    @Json(name = "over") val over: Boolean,
+    @Json(name = "pageCount") val pageCount: Int,
+    @Json(name = "size") val size: Int,
+    @Json(name = "total") val total: Int
+)
+
+//文章
+data class Article(
+    @Json(name = "apkLink") val apkLink: String,
+    @Json(name = "author") val author: String,
+    @Json(name = "chapterId") val chapterId: Int,
+    @Json(name = "chapterName") val chapterName: String,
+    @Json(name = "collect") var collect: Boolean,
+    @Json(name = "courseId") val courseId: Int,
+    @Json(name = "desc") val desc: String,
+    @Json(name = "envelopePic") val envelopePic: String,
+    @Json(name = "fresh") val fresh: Boolean,
+    @Json(name = "id") val id: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "niceDate") val niceDate: String,
+    @Json(name = "origin") val origin: String,
+    @Json(name = "projectLink") val projectLink: String,
+    @Json(name = "publishTime") val publishTime: Long,
+    @Json(name = "superChapterId") val superChapterId: Int,
+    @Json(name = "superChapterName") val superChapterName: String,
+    @Json(name = "tags") val tags: MutableList<Tag>,
+    @Json(name = "title") val title: String,
+    @Json(name = "type") val type: Int,
+    @Json(name = "userId") val userId: Int,
+    @Json(name = "visible") val visible: Int,
+    @Json(name = "zan") val zan: Int,
+    @Json(name = "top") var top: String
+)
+
+data class Tag(
+    @Json(name = "name") val name: String,
+    @Json(name = "url") val url: String
+)
+
+//轮播图
+data class Banner(
+    @Json(name = "desc") val desc: String,
+    @Json(name = "id") val id: Int,
+    @Json(name = "imagePath") val imagePath: String,
+    @Json(name = "isVisible") val isVisible: Int,
+    @Json(name = "order") val order: Int,
+    @Json(name = "title") val title: String,
+    @Json(name = "type") val type: Int,
+    @Json(name = "url") val url: String
+)
+
+data class HotKey(
+    @Json(name = "id") val id: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "visible") val visible: Int
+)
+
+//常用网站
+data class Friend(
+    @Json(name = "icon") val icon: String,
+    @Json(name = "id") val id: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "visible") val visible: Int
+)
+
+//知识体系
+data class KnowledgeTreeBody(
+    @Json(name = "children") val children: MutableList<Knowledge>,
+    @Json(name = "courseId") val courseId: Int,
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "parentChapterId") val parentChapterId: Int,
+    @Json(name = "visible") val visible: Int
+) : Serializable
+
+data class Knowledge(
+    @Json(name = "children") val children: List<Any>,
+    @Json(name = "courseId") val courseId: Int,
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "parentChapterId") val parentChapterId: Int,
+    @Json(name = "visible") val visible: Int
+) : Serializable
+
+// 登录数据
+data class LoginData(
+    @Json(name = "chapterTops") val chapterTops: MutableList<String>,
+    @Json(name = "collectIds") val collectIds: MutableList<String>,
+    @Json(name = "email") val email: String,
+    @Json(name = "icon") val icon: String,
+    @Json(name = "id") val id: Int,
+    @Json(name = "password") val password: String,
+    @Json(name = "token") val token: String,
+    @Json(name = "type") val type: Int,
+    @Json(name = "username") val username: String
+)
+
+//收藏网站
+data class CollectionWebsite(
+    @Json(name = "desc") val desc: String,
+    @Json(name = "icon") val icon: String,
+    @Json(name = "id") val id: Int,
+    @Json(name = "link") var link: String,
+    @Json(name = "name") var name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "userId") val userId: Int,
+    @Json(name = "visible") val visible: Int
+)
+
+
+data class CollectionResponseBody<T>(
+    @Json(name = "curPage") val curPage: Int,
+    @Json(name = "datas") val datas: List<T>,
+    @Json(name = "offset") val offset: Int,
+    @Json(name = "over") val over: Boolean,
+    @Json(name = "pageCount") val pageCount: Int,
+    @Json(name = "size") val size: Int,
+    @Json(name = "total") val total: Int
+)
+
+data class CollectionArticle(
+    @Json(name = "author") val author: String,
+    @Json(name = "chapterId") val chapterId: Int,
+    @Json(name = "chapterName") val chapterName: String,
+    @Json(name = "courseId") val courseId: Int,
+    @Json(name = "desc") val desc: String,
+    @Json(name = "envelopePic") val envelopePic: String,
+    @Json(name = "id") val id: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "niceDate") val niceDate: String,
+    @Json(name = "origin") val origin: String,
+    @Json(name = "originId") val originId: Int,
+    @Json(name = "publishTime") val publishTime: Long,
+    @Json(name = "title") val title: String,
+    @Json(name = "userId") val userId: Int,
+    @Json(name = "visible") val visible: Int,
+    @Json(name = "zan") val zan: Int
+)
+
+// 导航
+data class NavigationBean(
+    val articles: MutableList<Article>,
+    val cid: Int,
+    val name: String
+)
+
+// 项目
+data class ProjectTreeBean(
+    @Json(name = "children") val children: List<Any>,
+    @Json(name = "courseId") val courseId: Int,
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "parentChapterId") val parentChapterId: Int,
+    @Json(name = "visible") val visible: Int
+)
+
+// 热门搜索
+data class HotSearchBean(
+    @Json(name = "id") val id: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "visible") val visible: Int
+)
+
+// 搜索历史
+//data class SearchHistoryBean(val key: String) : LitePalSupport() {
+//    val id: Long = 0
+//}
+
+// TODO工具 类型
+data class TodoTypeBean(
+    val type: Int,
+    val name: String,
+    var isSelected: Boolean
+)
+
+// TODO实体类
+data class TodoBean(
+    @Json(name = "id") val id: Int,
+    @Json(name = "completeDate") val completeDate: String,
+    @Json(name = "completeDateStr") val completeDateStr: String,
+    @Json(name = "content") val content: String,
+    @Json(name = "date") val date: Long,
+    @Json(name = "dateStr") val dateStr: String,
+    @Json(name = "status") val status: Int,
+    @Json(name = "title") val title: String,
+    @Json(name = "type") val type: Int,
+    @Json(name = "userId") val userId: Int,
+    @Json(name = "priority") val priority: Int
+) : Serializable
+
+data class TodoListBean(
+    @Json(name = "date") val date: Long,
+    @Json(name = "todoList") val todoList: MutableList<TodoBean>
+)
+
+// 所有TODO，包括待办和已完成
+data class AllTodoResponseBody(
+    @Json(name = "type") val type: Int,
+    @Json(name = "doneList") val doneList: MutableList<TodoListBean>,
+    @Json(name = "todoList") val todoList: MutableList<TodoListBean>
+)
+
+data class TodoResponseBody(
+    @Json(name = "curPage") val curPage: Int,
+    @Json(name = "datas") val datas: MutableList<TodoBean>,
+    @Json(name = "offset") val offset: Int,
+    @Json(name = "over") val over: Boolean,
+    @Json(name = "pageCount") val pageCount: Int,
+    @Json(name = "size") val size: Int,
+    @Json(name = "total") val total: Int
+)
+
+// 新增TODO的实体
+data class AddTodoBean(
+    @Json(name = "title") val title: String,
+    @Json(name = "content") val content: String,
+    @Json(name = "date") val date: String,
+    @Json(name = "type") val type: Int
+)
+
+// 更新TODO的实体
+data class UpdateTodoBean(
+    @Json(name = "title") val title: String,
+    @Json(name = "content") val content: String,
+    @Json(name = "date") val date: String,
+    @Json(name = "status") val status: Int,
+    @Json(name = "type") val type: Int
+)
+
+// 公众号列表实体
+data class WXChapterBean(
+    @Json(name = "children") val children: MutableList<String>,
+    @Json(name = "courseId") val courseId: Int,
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "order") val order: Int,
+    @Json(name = "parentChapterId") val parentChapterId: Int,
+    @Json(name = "userControlSetTop") val userControlSetTop: Boolean,
+    @Json(name = "visible") val visible: Int
+)
+
+
+
